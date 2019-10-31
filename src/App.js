908ccbcc86f358eeb001a533962636ff46300dec
Jake Wilson <jakewilsonfl@gmail.com>
2019-10-31 17:34:37 -0500

    Add CardPlaceholder component.
    Add bootstrap lib.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
 import React from 'react';
 import './App.css';
+import './bootstrap.min.css'
 
 function Card(props) {
   return (
-    <div className="Card">
+    <div className="Card" draggable="true">
       <span>{props.value}</span>
     </div>
   );
 }
 
-function Deck() {
+function CardPlaceholder(props) {
   return (
-    <div className="Deck">
-      <Card value="Exploration" />
-      <Card value="Accountability" />
+    <div className="CardPlaceholder">
+      {props.name}
     </div>
   );
+}
+
+class Deck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: this.props.cards
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    let card = <CardPlaceholder name="Empty Deck"/>;
+    if (this.state.cards && this.state.cards.length > 0) {
+      card = <Card value={this.state.cards[0]}/>;
+    }
+    return (
+      <div className="Deck">
+        {card}
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
   render() {
     return (
       <div class="App">
-        <Deck />
+        <div class="row">
+          <Deck cards={getCards()}/>
+        </div>
+        <div class="row">
+          <Deck />
+          <Deck />
+          <Deck />
+        </div>
       </div>
     );
   }
 }
 
-export default Deck;
+function getCards() {
+  return [
+    'Love',
+    'Health',
+    'Wealth',
+    'Comfort',
+    'Fun',
+    'Happiness',
+    'Success',
+    'Learning',
+    'Peace',
+    'Intimacy',
+    'Adventure',
+    'Security'
+  ]
+}
+
+export default App;
